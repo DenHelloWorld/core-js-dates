5d3d95353f5b4dd9b1fbfea879d8d6e78f6ca23d
Denis Saveliev <denissaveliev23@gmail.com>
2024-03-26 00:35:10 +0200

    feat: solve last task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -282,8 +282,14 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+
+  const isDivisible4 = year % 4 === 0;
+  const isDivisible100 = year % 100 === 0;
+  const isDivisible400 = year % 400 === 0;
+
+  return (isDivisible4 && !isDivisible100) || isDivisible400;
 }
 
 module.exports = {
